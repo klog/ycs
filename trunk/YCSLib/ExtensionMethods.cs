cd00e28778aff1cdf12fc9bfedf00e80500cd782
wicked <wickedcoder@hotmail.com>
2011-01-18 07:11:23 +0000

    + refactored
    git-svn-id: https://ycs.svn.sourceforge.net/svnroot/ycs@10 fbea4271-1f72-4fe8-adcb-f1cb204d06b1

---
+++ trunk/YCSLib/ExtensionMethods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace YCSLib
+{
+    public static class ExtensionMethods
+    {
+        public static YMSGPacketPayload Slice(this YMSGPacketPayload payload, string key, int index)
+        {
+            YMSGPacketPayload retVal = null;
+            int x = 0;
+            for (int i = 0; i < payload.Count; i++)
+                if (payload[index].Key == key)
+                    if (x == index)
+                        retVal = new YMSGPacketPayload(payload.GetRange(x, payload.FindIndex(x, p =>
+                        { if (p.Key == key) return true; return false; }
+                        )));
+                    else
+                        x++;
+            return retVal;
+        }
+
+        [DebuggerStepThrough]
+        internal static unsafe int FindIndex(this byte[] bytes, byte delimeter, int startIndex = 0)
+        {
+            fixed (byte* pBytes = bytes)
+            {
+                int i = startIndex;
+                while (i < bytes.Length)
+                {
+                    if (*(pBytes + i) == delimeter)
+                        return (i - startIndex);
+                    i++;
+                }
+            }
+
+            return -1;
+        }
+
+        internal static byte[] Slice(this byte[] bytes, int length, int startIndex = 0)
+        {
+            return bytes.Skip(startIndex).Take(length).ToArray();
+        }
+    }
+}
--- trunk/YCSLib/YMSGPacket.cs
+++ trunk/YCSLib/YMSGPacket.cs
@@ -7,150 +7,8 @@ using System.Diagnostics;
 
 namespace YCSLib
 {
-    public static class ExtensionMethods
-    {
-        public static YCSLib.YMSGPacket.YMSGPacketPayload Slice(this YCSLib.YMSGPacket.YMSGPacketPayload payload, string key, int index)
-        {
-            YCSLib.YMSGPacket.YMSGPacketPayload retVal = null;
-            int x = 0;
-            for (int i = 0; i < payload.Count; i++)
-                if (payload[index].Key == key)
-                    if (x == index)
-                        retVal = new YCSLib.YMSGPacket.YMSGPacketPayload(payload.GetRange(x, payload.FindIndex(x, p =>
-                        { if (p.Key == key) return true; return false; }
-                        )));
-                    else
-                        x++;
-            return retVal;
-        }
-
-        [DebuggerStepThrough]
-        internal static unsafe int FindIndex(this byte[] bytes, byte delimeter, int startIndex = 0)
-        {
-            fixed (byte* pBytes = bytes)
-            {
-                int i = startIndex;
-                while (i < bytes.Length)
-                {
-                    if (*(pBytes + i) == delimeter)
-                        return (i - startIndex);
-                    i++;
-                }
-            }
-
-            return -1;
-        }
-
-        internal static byte[] Slice(this byte[] bytes, int length, int startIndex = 0)
-        {
-            return bytes.Skip(startIndex).Take(length).ToArray();
-        }
-    }
-
-
     public class YMSGPacket : EventArgs
     {
-        public class YMSGPacketPayload : List<KeyValuePair<string, string>>
-        {
-            public static readonly byte[] YMSGDelimeter = new byte[] { (byte)0xc0, (byte)0x80 };
-
-            public YMSGPacketPayload()
-            {
-
-            }
-
-            public YMSGPacketPayload(IEnumerable<KeyValuePair<string, string>> balls)
-                : base(balls)
-            {
-
-            }
-
-            [MethodImpl(MethodImplOptions.Synchronized)]
-            public byte[] ToBytes()
-            {
-                List<byte> retVal = new List<byte>();
-
-                foreach (KeyValuePair<string, string> kv in this)
-                {
-                    byte[] k = GetEncoding().GetBytes(kv.Key);
-                    byte[] j = GetEncoding().GetBytes(kv.Value);
-                    retVal.AddRange(k);
-                    retVal.AddRange(YMSGDelimeter);
-                    retVal.AddRange(j);
-                    retVal.AddRange(YMSGDelimeter);
-                }
-
-                return retVal.ToArray();
-            }
-            /// <summary>
-            /// indexer...
-            /// </summary>
-            /// <param name="key"></param>
-            /// <returns></returns>
-            public string this[string key]
-            {
-                get
-                {
-                    return base.Find(k => { if (k.Key == key) return true; return false; }).Value;
-                }
-                set
-                {
-                    base.Add(new KeyValuePair<string, string>(key, value));
-                }
-            }
-
-            /// <summary>
-            /// returns a set of K,V pairs for the specified index of its occurance...UNPOSSIBLE!
-            /// </summary>
-            /// <param name="key"></param>
-            /// <param name="index"></param>
-            /// <returns></returns>
-            public YMSGPacketPayload this[string key, int index]
-            {
-                get
-                {
-                    var all = YMSGPacketPayload.GetChunk(this, key, null);
-                    return all.Slice(key, index);
-                }
-                set
-                {
-                    this.AddRange(value);
-                }
-            }
-
-            private static YMSGPacketPayload GetChunk(YMSGPacketPayload payload, string startKey, string[] endKey = null)
-            {
-                YMSGPacketPayload retVal = new YMSGPacketPayload();
-
-                for (int i = 0; i < payload.Count; i++)
-                    if (endKey == null)
-                        if (payload[i].Key == startKey)
-                        {
-                            retVal.AddRange(payload.GetRange(i, payload.Count - i));
-                            break;
-                        }
-                        else
-                        {
-                            retVal.AddRange(payload.GetRange(i, payload.FindIndex(i, payload.Count - i,
-                                x => { if (endKey.Contains<string>(x.Key)) return true; return false; })));
-                            break;
-                        }
-                return retVal;
-            }
-
-            /// <summary>
-            /// returns the sequence of k,v pairs with the specified start and end keys
-            /// </summary>
-            /// <param name="startKey"></param>
-            /// <param name="endKey"></param>
-            /// <returns></returns>
-            public YMSGPacketPayload GetChunk(string startKey, string[] endKey = null)
-            {
-                return GetChunk(this, startKey, endKey);
-            }
-
-        }
-
         public static bool QuirksMode = false;
 
         public YMSGPacket()
@@ -269,7 +127,7 @@ namespace YCSLib
             }
         }
 
-        private static Encoding GetEncoding()
+        internal static Encoding GetEncoding()
         {
             return (QuirksMode ? Encoding.GetEncoding("ISO-8859-1") : Encoding.UTF8);
         }
